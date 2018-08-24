5e47d25bb77aeacebc9b9ca8c0beb229968c59b0
Felix von Drigalski <FvDrigalski@gmail.com>
2018-08-24 20:52:56 +0900

    Insert missed timeout check, prettify

--- include/ur_modern_driver/wait_for_program.h
+++ include/ur_modern_driver/wait_for_program.h
@@ -7,19 +7,15 @@
 // Note: topic_namespace needs a leading slash.
 bool isProgramRunning(std::string topic_namespace = "")
 {
-  boost::shared_ptr<ur_modern_driver::RobotModeDataMsg const> sharedPtr;
   ur_modern_driver::RobotModeDataMsg robot_mode_state;
-  sharedPtr  = ros::topic::waitForMessage<ur_modern_driver::RobotModeDataMsg>(topic_namespace + "/ur_driver/robot_mode_state", ros::Duration(2));
-  if (sharedPtr == NULL)
-  {
-    ROS_ERROR("No message received from the robot. Is everything running? Is the namespace set correctly?");
-    ros::Exception e("No message received from the robot.");
-    throw(e);
-  }
+  auto msg = ros::topic::waitForMessage<ur_modern_driver::RobotModeDataMsg>(topic_namespace + "/ur_driver/robot_mode_state", ros::Duration(2));
+  if (msg)
+    return msg->is_program_running;
   else
   {
-    robot_mode_state = *sharedPtr;
-    return robot_mode_state.is_program_running;
+    ROS_ERROR("No message received from the robot. Is everything running? Is the namespace entered correctly with a leading slash?");
+    ros::Exception e("No message received from the robot.");
+    throw(e);
   }
 }
 
@@ -27,10 +23,12 @@ bool isProgramRunning(std::string topic_namespace = "")
 bool waitForURProgram(std::string topic_namespace = "", ros::Duration timeout = ros::Duration(60.0))
 {
   ROS_INFO("Waiting for UR program to finish. Only run this after sending custom URScripts and not the regular motion commands, or this call will not terminate before the timeout.");
-  ros::Time t_now, t_start = ros::Time::now();
-  while (isProgramRunning(topic_namespace))
+  ros::Time t_start = ros::Time::now();
+  ros::Duration time_passed = ros::Time::now() - t_start;
+  while ( isProgramRunning(topic_namespace) && (time_passed < timeout) )
   {
     ros::Duration(.05).sleep();
+    time_passed = ros::Time::now() - t_start;
   }
   ROS_INFO("UR Program has terminated.");
   return true;
