0983f83bdbc49a7fcf9e629f35127300c1421c15
Felix von Drigalski <FvDrigalski@gmail.com>
2018-10-11 22:03:49 +0900

    Decrease verbosity

--- include/ur_modern_driver/wait_for_program.h
+++ include/ur_modern_driver/wait_for_program.h
@@ -22,7 +22,7 @@ bool isProgramRunning(std::string topic_namespace = "")
 // Note: topic_namespace needs a leading slash.
 bool waitForURProgram(std::string topic_namespace = "", ros::Duration timeout = ros::Duration(60.0))
 {
-  ROS_INFO("Waiting for UR program to finish. Only run this after sending custom URScripts and not the regular motion commands, or this call will not terminate before the timeout.");
+  ROS_DEBUG("Waiting for UR program to finish. Only run this after sending custom URScripts and not the regular motion commands, or this call will not terminate before the timeout.");
   ros::Time t_start = ros::Time::now();
   ros::Duration time_passed = ros::Time::now() - t_start;
   while ( isProgramRunning(topic_namespace) && (time_passed < timeout) )
@@ -30,6 +30,6 @@ bool waitForURProgram(std::string topic_namespace = "", ros::Duration timeout = 
     ros::Duration(.05).sleep();
     time_passed = ros::Time::now() - t_start;
   }
-  ROS_INFO("UR Program has terminated.");
+  ROS_DEBUG("UR Program has terminated.");
   return true;
 }
