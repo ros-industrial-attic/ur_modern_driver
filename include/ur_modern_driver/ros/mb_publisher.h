246d8147ead3adf910db04a4b740411a53c4f547
Felix von Drigalski <FvDrigalski@gmail.com>
2018-12-19 11:05:39 +0900

    clang format

--- include/ur_modern_driver/ros/mb_publisher.h
+++ include/ur_modern_driver/ros/mb_publisher.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #include <industrial_msgs/RobotStatus.h>
-#include <std_msgs/Bool.h>
 #include <ros/ros.h>
+#include <std_msgs/Bool.h>
 #include <ur_msgs/Analog.h>
 #include <ur_msgs/Digital.h>
 #include <ur_msgs/IOStates.h>
@@ -40,10 +40,11 @@ private:
   void publishRobotStatus(const RobotModeData_V3_0__1& data) const;
 
 public:
-  MBPublisher() : io_pub_(nh_.advertise<ur_msgs::IOStates>("ur_driver/io_states", 1))
-                , status_pub_(nh_.advertise<industrial_msgs::RobotStatus>("ur_driver/robot_status", 1))
-                , masterboard_state_pub_(nh_.advertise<ur_msgs::MasterboardDataMsg>("ur_driver/masterboard_state", 1))
-                , robot_mode_state_pub_(nh_.advertise<ur_msgs::RobotModeDataMsg>("ur_driver/robot_mode_state", 1))
+  MBPublisher()
+    : io_pub_(nh_.advertise<ur_msgs::IOStates>("ur_driver/io_states", 1))
+    , status_pub_(nh_.advertise<industrial_msgs::RobotStatus>("ur_driver/robot_status", 1))
+    , masterboard_state_pub_(nh_.advertise<ur_msgs::MasterboardDataMsg>("ur_driver/masterboard_state", 1))
+    , robot_mode_state_pub_(nh_.advertise<ur_msgs::RobotModeDataMsg>("ur_driver/robot_mode_state", 1))
   {
   }
 
--- include/ur_modern_driver/wait_for_program.h
+++ include/ur_modern_driver/wait_for_program.h
@@ -8,12 +8,14 @@
 bool isProgramRunning(std::string topic_namespace = "")
 {
   ur_msgs::RobotModeDataMsg robot_mode_state;
-  auto msg = ros::topic::waitForMessage<ur_msgs::RobotModeDataMsg>(topic_namespace + "/ur_driver/robot_mode_state", ros::Duration(2));
+  auto msg = ros::topic::waitForMessage<ur_msgs::RobotModeDataMsg>(topic_namespace + "/ur_driver/robot_mode_state",
+                                                                   ros::Duration(2));
   if (msg)
     return msg->is_program_running;
   else
   {
-    ROS_ERROR("No message received from the robot. Is everything running? Is the namespace entered correctly with a leading slash?");
+    ROS_ERROR("No message received from the robot. Is everything running? Is the namespace entered correctly with a "
+              "leading slash?");
     ros::Exception e("No message received from the robot.");
     throw(e);
   }
@@ -22,10 +24,11 @@ bool isProgramRunning(std::string topic_namespace = "")
 // Note: topic_namespace needs a leading slash.
 bool waitForURProgram(std::string topic_namespace = "", ros::Duration timeout = ros::Duration(60.0))
 {
-  ROS_DEBUG("Waiting for UR program to finish. Only run this after sending custom URScripts and not the regular motion commands, or this call will not terminate before the timeout.");
+  ROS_DEBUG("Waiting for UR program to finish. Only run this after sending custom URScripts and not the regular motion "
+            "commands, or this call will not terminate before the timeout.");
   ros::Time t_start = ros::Time::now();
   ros::Duration time_passed = ros::Time::now() - t_start;
-  while ( isProgramRunning(topic_namespace) && (time_passed < timeout) )
+  while (isProgramRunning(topic_namespace) && (time_passed < timeout))
   {
     ros::Duration(.05).sleep();
     time_passed = ros::Time::now() - t_start;
